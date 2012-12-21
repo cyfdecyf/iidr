45dac870f61b4864128ca894ca6704ec508b4557
GlacJAY <glacjay@gmail.com>
2012-12-21 10:44:20 +0800

    add simple cache support

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/pmylund/go-cache"
 	"net"
 	"os"
 	"strings"
@@ -15,12 +16,22 @@ type reply struct {
 }
 
 var (
-	server  *net.UDPAddr
-	replyCh chan *reply
-	count   int32
+	server   *net.UDPAddr
+	replyCh  chan *reply
+	theCache *cache.Cache
+	count    int32
 )
 
 func main() {
+	theCache = cache.New(time.Hour, time.Minute)
+
+	var err error
+	server, err = net.ResolveUDPAddr("udp", "208.67.220.220:53")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Can't resolve server address: %v\n", err)
+		os.Exit(1)
+	}
+
 	addr, err := net.ResolveUDPAddr("udp", "0.0.0.0:53")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Can't resolve service address: %v\n", err)
@@ -31,13 +42,10 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Can't listen on udp port: %v\n", err)
 		os.Exit(1)
 	}
+
 	replyCh = make(chan *reply)
 	go sendReply(conn)
-	server, err = net.ResolveUDPAddr("udp", "8.8.4.4:53")
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Can't resolve server address: %v\n", err)
-		os.Exit(1)
-	}
+
 	for {
 		reqData := make([]byte, 2048)
 		nr, client, err := conn.ReadFromUDP(reqData)
@@ -58,39 +66,21 @@ func checkTempErr(err error) {
 }
 
 func proxyClientDNS(reqData []byte, client *net.UDPAddr) {
-	c := atomic.AddInt32(&count, 1)
 	domain := extractDomain(reqData)
+
+	c := atomic.AddInt32(&count, 1)
 	fmt.Printf("(%3d) Receive DNS request from %24v: %v\n", c, client, domain)
 
-	conn, err := net.DialUDP("udp", nil, server)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Can't dial server: %v\n", err)
-		os.Exit(1)
-	}
-	defer conn.Close()
-	_, err = conn.Write(reqData)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to write to server: %v\n", err)
-		return
-	}
-	t := time.Now().Add(time.Duration(5) * time.Second)
-	conn.SetReadDeadline(t)
+	item, found := theCache.Get(domain)
 	var rep []byte
-	for {
-		repData := make([]byte, 2048)
-		nr, err := conn.Read(repData)
-		if err != nil {
-			err2, ok := err.(*net.OpError)
-			if ok && err2.Timeout() {
-				break
-			}
-			fmt.Fprintf(os.Stderr, "Failed to read from server: %v\n", err)
-			break
-		}
-		rep = repData[:nr]
+	if found {
+		rep = item.([]byte)
+	} else {
+		rep = doProxyClientDNS(reqData, client)
+		theCache.Add(domain, rep, 0)
 	}
-	replyCh <- &reply{rep, client}
 
+	replyCh <- &reply{rep, client}
 	atomic.AddInt32(&count, -1)
 }
 
@@ -108,13 +98,44 @@ func extractDomain(reqData []byte) string {
 	return strings.Join(nodes, ".")
 }
 
+func doProxyClientDNS(reqData []byte, client *net.UDPAddr) []byte {
+	conn, err := net.DialUDP("udp", nil, server)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Can't dial server: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+	_, err = conn.Write(reqData)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to write to server: %v\n", err)
+		return nil
+	}
+	t := time.Now().Add(time.Duration(5) * time.Second)
+	conn.SetReadDeadline(t)
+	var rep []byte
+	for {
+		repData := make([]byte, 2048)
+		nr, err := conn.Read(repData)
+		if err != nil {
+			err2, ok := err.(*net.OpError)
+			if ok && err2.Timeout() {
+				break
+			}
+			fmt.Fprintf(os.Stderr, "Failed to read from server: %v\n", err)
+			break
+		}
+		rep = repData[:nr]
+	}
+	return rep
+}
+
 func sendReply(conn *net.UDPConn) {
 	for {
 		aReply := <-replyCh
 		_, err := conn.WriteToUDP(aReply.data, aReply.addr)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Failed to write to client: %v\n", err)
-			checkTempErr(err)
+			//checkTempErr(err)
 		}
 	}
 }
